0d2c977f3afb6a6776c9ccc7dcb07eae9741fa9f
Alexander Martin <hi@droqen.com>
2024-04-06 16:27:18 -0400

    fixed a wind glitch.

--- apr05-a-glade-without-leaves/strong_wind.gd
+++ apr05-a-glade-without-leaves/strong_wind.gd
@@ -11,13 +11,13 @@ func _physics_process(_delta):
 	if gust > 0:
 		var wind_strength = gust
 		if direction < 0:
-			if player.position.x < 10.0:
+			if player.position.x > 10.0:
 				wind_strength *= max(0, player.position.x / 10.0)
 		if direction > 0:
 			if 180 - player.position.x > 10.0:
 				wind_strength *= max(0, (180 - player.position.x) / 10.0)
-		player.velocity.x += 0.1 * direction + wind_strength * 0.15
-		gust *= 0.95
+		player.velocity.x += direction * (0.04 + wind_strength * 0.06)
+		gust *= 0.93
 		gust -= 0.01
 	if dies_out_after_intro and intro.is_done():
 		particles.emitting = false
